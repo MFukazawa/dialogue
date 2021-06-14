bb18d04de6a1827ccbabb91dbad3e80b0b598751
MFukazawa <micahfukazawa@gmail.com>
2021-06-14 22:48:31 +0900

    Add replay button

--- src/components/AudioControls.js
+++ src/components/AudioControls.js
@@ -5,13 +5,25 @@ const AudioControlsContainer = styled.div`
   width: 100%;
 `
 
-const AudioControls = ({ audioRef, isPlaying, onSkipForward, onSkipBack, togglePlay }) => {
+const AudioControls = ({ audioRef, isPlaying, togglePlay, hasEnded }) => {
   const togglePlayPause = (isPlaying) => {
-    return isPlaying ? togglePlay(false) : togglePlay(true)
+    return isPlaying ? togglePlay(false) : togglePlay(true);
   };
 
   const skip = (dataset) => {
     audioRef.current.currentTime += parseFloat(dataset.skip)
+  };
+
+  const playButtonIcon = () => {
+    if (!hasEnded) {
+      return isPlaying
+        ? <img src="https://s2.svgbox.net/materialui.svg?ic=pause" width="32" height="32" />
+        : <img src="https://s2.svgbox.net/materialui.svg?ic=play_arrow" width="32" height="32" />
+    } else {
+      return isPlaying
+        ? <img src="https://s2.svgbox.net/materialui.svg?ic=pause" width="32" height="32" />
+        : <img src="https://s2.svgbox.net/materialui.svg?ic=replay" width="32" height="32" />
+    }
   };
 
   return (
@@ -21,10 +33,7 @@ const AudioControls = ({ audioRef, isPlaying, onSkipForward, onSkipBack, toggleP
       </div>
 
       <button className="audio-play" onClick={() => togglePlayPause(isPlaying)}>
-        { isPlaying
-          ? <img src="https://s2.svgbox.net/materialui.svg?ic=pause" width="32" height="32" />
-          : <img src="https://s2.svgbox.net/materialui.svg?ic=play_arrow" width="32" height="32" />
-        }
+        {playButtonIcon()}
       </button>
       {/* <input type="range" name="volume" className="player__slider" min="0" max="1" step="0.05" value="1" /> */}
       {/* <input type="range" name="playbackRate" class="player__slider" min="0.5" max="2" step="0.1" value="1" /> */}
--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -6,6 +6,7 @@ import background from '../assets/images/background_1.png';
 const AudioPlayer = ({ track }) => {
   const [trackProgress, setTrackProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [hasEnded, setHasEnded] = useState(false);
 
   const audioRef = useRef(new Audio(audioSrc));
   const intervalRef = useRef();
@@ -36,25 +37,14 @@ const AudioPlayer = ({ track }) => {
 
 	  intervalRef.current = setInterval(() => {
 	    if (audioRef.current.ended) {
-	      // toNextTrack();
-        console.log('ended')
+        setIsPlaying(false);
+        setHasEnded(true);
 	    } else {
 	      // setTrackProgress(audioRef.current.currentTime);
         console.log('track progress set')
 	    }
 	  }, [1000]);
 	}
-  
-  const onSkipBack = () => {
-    clearInterval(intervalRef.current);
-    // audioRef.current.currentTime = value;
-    console.log(audioRef.current.currentTime)
-    console.log('go back 10 seconds');
-  };
-
-  const onSkipForward = () => {
-    console.log('go forward 10 seconds')
-  }
 
   return (
     <div className="audio-player">
@@ -65,10 +55,9 @@ const AudioPlayer = ({ track }) => {
       />
       <AudioControls
         isPlaying={isPlaying}
-        onRewind={onSkipForward}
-        onFastForward={onSkipBack}
         togglePlay={setIsPlaying}
         audioRef={audioRef}
+        hasEnded={hasEnded}
       />
     </div>
   );
